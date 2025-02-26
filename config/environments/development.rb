2c1d424acbf5b933fbb4ed56e43a6d838d829732
nabe <197488176+nabe-jp@users.noreply.github.com>
2025-02-26 06:25:54 +0000

    2日目

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.hosts.clear # Railsを学ぼう 2章 ページの確認をする 最後の方にどのホストからでもアクセスできるよう追加
+  config.active_job.queue_adapter = :inline
 end
---
+++ db/migrate/20250225234914_create_books.rb
@@ -0,0 +1,17 @@
+class CreateBooks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :books do |t|
+
+      ## 本のタイトル
+      t.string :title
+
+      ## 感想
+      t.text :body
+              
+      ## ユーザーID
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
