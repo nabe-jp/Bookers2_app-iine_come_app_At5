0c24253ebc9d24f39f7de5d71288f27b63b8274e
nabe <197488176+nabe-jp@users.noreply.github.com>
2025-03-09 08:15:42 +0000

    Bookers2_03/09

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,7 +5,7 @@ class BooksController < ApplicationController
     @book.user_id = current_user.id
     if @book.save
       flash[:notice] = "You have created book successfully."
-      redirect_to book_path(@book.user_id)
+      redirect_to book_path(@book.id)
     else
       @user = current_user
       @books = Book.page(params[:page])
